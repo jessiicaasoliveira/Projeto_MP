1afb05c62bacd5c8a8d034627c3946f7b8368388
Amanda Aline <amanda.carvalho@redes.unb.br>
2018-12-01 17:52:31 -0200

    Adicionando checagem dos jogadores

--- src/LogicaJogo.c
+++ src/LogicaJogo.c
@@ -55,3 +55,34 @@ void startNewGame()
 
    destroyGame();
 }
+
+/**
+ **************************************************************************
+ * @brief Função: Verificar Status das Estruturas e tropas do Player
+ *
+ * Descrição:
+ * A Função chama as funções para verificar se há batalhas entre tropas, se alguma tropa está atacando algum prédio ou base inimiga, qual a vida dos prédios do player e a quantidade de tropas que o player possui.
+ *
+ * Parâmetros:
+ * @param sem parâmetros
+ *
+ * Valor retornado:
+ * @return função void - A função não retorna nada
+ *
+ * Assertiva de entrada:
+ * Sem parâmetros
+ *
+ * Assertiva de saída:
+ * Função void
+ ***************************************************************************/
+void playerCheckers()
+{
+    battleChecker(&T1_p, &T1_e, &T2_e, &T3_e);
+    battleChecker(&T2_p, &T1_e, &T2_e, &T3_e);
+    battleChecker(&T3_p, &T1_e, &T2_e, &T3_e);
+    destroyBuildChecker(&T1_p, &Base_e, &B1_e, &B2_e, &B3_e);
+    destroyBuildChecker(&T2_p, &Base_e, &B1_e, &B2_e, &B3_e);
+    destroyBuildChecker(&T3_p, &Base_e, &B1_e, &B2_e, &B3_e);
+    buildHPChecker(&B1_p, &B2_p, &B3_p, map1);
+    troopAmountChecker(&T1_p, &T2_p, &T3_p, map1);
+}
