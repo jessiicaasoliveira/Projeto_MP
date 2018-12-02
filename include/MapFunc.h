ec25d838d1b23d8db52e1100045a9a50aba8318b
Jessica Oliveira <130028983@aluno.unb.br>
2018-12-01 22:56:50 -0200

    Prototipo da função setBaseOnMap  criado

--- include/MapFunc.h
+++ include/MapFunc.h
@@ -8,3 +8,5 @@ char** map1;
 char** allocMap(char** map, int y, int x);
 
 char** deallocMap(char** map);
+
+void setBaseOnMap(char** map, Base* bc, int x, int y, int width, int height, int who, int save);
