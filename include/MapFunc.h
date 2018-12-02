5d463c5357f40cd52efc4d777f99a22327557db3
Jessica Oliveira <130028983@aluno.unb.br>
2018-12-01 23:07:03 -0200

    Prototipo da função setMap  criado

--- include/MapFunc.h
+++ include/MapFunc.h
@@ -12,3 +12,7 @@ char** deallocMap(char** map);
 void setBaseOnMap(char** map, Base* bc, int x, int y, int width, int height, int who, int save);
 
 void setBuildOnMap(char** map, Build* b, int x, int y, int width, int height, int type, int who, int save);
+
+void setTroopOnMap(char** map, Troop* t, int x, int y, int type, int who, int save);
+
+void setMap(char** map, int save);
