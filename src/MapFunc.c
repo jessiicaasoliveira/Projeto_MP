ea38237ff135919d0adf2d5a2e93f9869b1c0a93
Jessica Oliveira <130028983@aluno.unb.br>
2018-12-01 22:57:15 -0200

    Função setBaseOnMap criada

--- src/MapFunc.c
+++ src/MapFunc.c
@@ -28,6 +28,7 @@ int map_x;
  * Assertiva de saída:
  * char** map != NULL
  ***************************************************************************/
+
 char** allocMap(char** map, int y, int x) {
     FILE* fp;
     int i;
@@ -54,7 +55,9 @@ char** allocMap(char** map, int y, int x) {
 
     return map;
 }
-**************************************************************************
+
+/**
+ **************************************************************************
  * @brief Função: Desalocar o Mapa do Jogo
  *
  * Descrição:
@@ -71,7 +74,8 @@ char** allocMap(char** map, int y, int x) {
  *
  * Assertiva de saída:
  * char** map == NULL
- ***************************************************************************/
+***************************************************************************/
+
 char** deallocMap(char** map) {
     FILE* fp;
     int i;
@@ -92,4 +96,55 @@ char** deallocMap(char** map) {
     return map;
 }
 
+/**
+ **************************************************************************
+ * @brief Função: Colocar no Mapa a Base
+ *
+ * Descrição:
+ * A Função recebe uma matriz (mapa do jogo) do tipo char, uma struct de uma base, a posição que essa base vai ficar, a altura e a largura dessa base, uma flag para identificar se a base é do player ou do PC e uma flag para identificar se a base é de um jogo novo ou de um save. A base é setada e escrita no mapa como 'B' ou 'C', dependendo da flag, na posição que foi mandada.
+ *
+ * Parâmetros:
+ * @param char** map - matriz (mapa) do tipo char
+ * @param Base* bc - uma struct para a base
+ * @param int x - coordenada x da base
+ * @param int y - coordenada y da base
+ * @param int width - largura da base
+ * @param int height - altura da base
+ * @param int who - flag para identificar se a base é do Player (0) ou do PC (1)
+ * @param int save - flag para identificar se a base está sendo carregada de um jogo novo (0) ou de um save (1)
+ *
+ * Valor retornado:
+ * @return função void. Os valores são mudados por referência
+ *
+ * Assertiva de entrada:
+ * char** map != NULL
+ * Base* bc != NULL
+ * int x >= 0
+ * int y >= 0
+ * int width > 0
+ * int height > 0
+ * int who == 0 || who == 1
+ * int save == 0 || save == 1
+ *
+ * Assertiva de saída:
+ * Função void
+ ***************************************************************************/
+void setBaseOnMap(char** map, Base* bc, int x, int y, int width, int height, int who, int save) {
+    int i, j;
+
+    if (!save) {
+        bc = setBaseAtt(bc, x, y, width, height);
+    }
+
+    for (i = bc->y; i <= (bc->height + bc->y); i++)
+        for (j = bc->x; j <= (bc->width + bc->x); j++) {
+            switch (who) {
+            case 0: map[i][j] = 'X';
+                break;
+
+            case 1: map[i][j] = 'Y';
+                break;
+            }
+        }
+}
  
