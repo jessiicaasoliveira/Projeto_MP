88213b4aac9121e2233e47881d89074452b86848
Bruno Justino <bjpraciano@gmail.com>
2018-12-02 13:27:28 -0200

    Corrindo os erros do teste

---
+++ src/Tests.c
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN   
+#include "Catch.hpp"
+#include "EnemyFunc.h"
+#include "GameLogic.h"
+#include "MapFunc.h"
+#include "PlayerFunc.h"
+#include "WinManager.h"
+
+/* Teste case para biblioteca EnemyFunc.h */
+TEST_CASE("Testes da biblioteca EnemyFunc.h:"){
+
+    int path_y;
+    path_y = ReadPathY(1, 1)
+
+}
